8243ded644783ac50720a75f92a073b74105e459
khanka786 <1khankashan9@gmail.com>
2025-05-19 19:08:19 -0400

    Updates

---
+++ precache-manifest.1cb8d3223923b92a2d541ec231244d95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618265529d8c6fec8dff180f23c2f8b3",
+    "url": "/Worlde-Style-Game/index.html"
+  },
+  {
+    "revision": "a95b91947243c80e5375",
+    "url": "/Worlde-Style-Game/static/css/main.4039770b.chunk.css"
+  },
+  {
+    "revision": "919360b0758f55a225c5",
+    "url": "/Worlde-Style-Game/static/js/2.c5cb5402.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/Worlde-Style-Game/static/js/2.c5cb5402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a95b91947243c80e5375",
+    "url": "/Worlde-Style-Game/static/js/main.e7e42ee7.chunk.js"
+  },
+  {
+    "revision": "c8499d3326422f796667",
+    "url": "/Worlde-Style-Game/static/js/runtime-main.3c7226d9.js"
+  },
+  {
+    "revision": "997dbe1ba092500dd50f78c394880086",
+    "url": "/Worlde-Style-Game/static/media/logo.997dbe1b.svg"
+  }
+]);
